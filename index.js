d85133d37d87536f8f86f9bfd3ca0441687c45dc
nichenqin1001 <nichenqin@hotmail.com>
2017-06-30 11:16:11 +0800

    first screen

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 (function () {
-  var resume = new Wall('#wrapper');
+  var resume = new Wall('#main');
+  var welcomeMain = document.querySelector('.welcome__main');
+  var welcomeNext = document.querySelector('.welcome__next');
+
+  welcomeNext.addEventListener('click', function () {
+    resume.nextSection()
+  });
+
+  window.onload = function () {
+    setTimeout(function () {
+      welcomeMain.classList.add('showcase');
+      welcomeNext.classList.add('showcase');
+    }, 2000)
+  }
 })()
